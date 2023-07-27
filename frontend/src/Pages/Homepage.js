1cc79ac426f07dd4a069009ce7ccf9f07beb2fc3
nihal866 <nihaldwivedi111@gmail.com>
2023-07-27 20:04:46 +0530

    Completed login and signup page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Container, Box, Text, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text textAlign="center" fontSize="4xl" fontFamily="work sans">
+          Talk-A-Bit
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded" colorScheme="green">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
