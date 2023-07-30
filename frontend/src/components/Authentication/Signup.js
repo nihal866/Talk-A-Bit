9655fef76863a995826c2f4b6f1bb2974904ba07
nihal866 <nihaldwivedi111@gmail.com>
2023-07-30 21:32:23 +0530

    Picture upload from cloudinary

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -6,6 +6,7 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 
@@ -16,9 +17,52 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleClick = () => setShow(!show);
-  const postDetails = (pics) => {};
+  const postDetails = (pics) => {
+    setLoading(true);
+    if(pics === undefined){
+      toast({
+        title: "Please select an image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if(pics.type === "image/jpeg" || pics.type === "image/png"){
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "dvcq338eo");
+      fetch("https://api.cloudinary.com/v1_1/dvcq338eo/image/upload", {
+        method: 'post',
+        body: data,
+      }).then((res) => res.json())
+      .then(data => {
+        setPic(data.url.toString());
+        console.log(data.url.toString());
+        setLoading(false);
+      }).catch((err) => {
+        console.log(err);
+        setLoading(false);
+      })
+    } else {
+      toast({
+        title: "Please select an image :)",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
   const submitHandler = () => {};
 
   return (
@@ -81,6 +125,7 @@ const Signup = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
