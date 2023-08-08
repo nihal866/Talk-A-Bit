6a586114abf368b119d552ed9edaa59a1233a798
nihal866 <nihaldwivedi111@gmail.com>
2023-08-08 10:46:20 +0530

    created group chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -56,10 +56,11 @@ const fetchChats = asyncHandler(async (req, res) => {
       .populate("users", "-password")
       .populate("groupAdmin", "-password")
       .populate("latestMessage")
-      .sort({ updatedAt: -1 }).then(async (results) => {
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
         results = await User.populate(results, {
-          path: 'latestMessage.sender',
-          select: 'name pic email',
+          path: "latestMessage.sender",
+          select: "name pic email",
         });
         res.status(200).send(results);
       });
@@ -69,4 +70,37 @@ const fetchChats = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat, fetchChats };
+const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please fill all the fields" });
+  }
+
+  var users = JSON.parse(req.body.users);
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send("More than 2 users are required to form a group");
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+      res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat };
